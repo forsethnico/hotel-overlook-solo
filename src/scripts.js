4cad1425fbfbe2aac8fd40b42b07a1aeae57efec
Nicole Forseth <forseth.nico@gmail.com>
2022-08-07 21:09:50 -0600

    Display available rooms to DOM

--- src/scripts.js
+++ src/scripts.js
@@ -32,6 +32,8 @@ const profilePage = document.querySelector(".profile-page");
 const loginForm = document.querySelector(".login-form");
 const checkAvailSection = document.querySelector(".check-availability");
 const availableRoomsSection = document.querySelector(".available-rooms");
+const roomTypeChoice = document.getElementById("selectRoomType");
+const datePicked = document.getElementById("arrivalDate");
 
 //---Event Listeners---//
 window.addEventListener("load", getHotelData);
@@ -190,17 +192,20 @@ function showBookingPage() {
 }
 
 function checkAvailability() {
-  let availableRooms = hotel.getAvailableRooms(date, rooms, bookings, roomType);
-  availableRoomsSection.innerHTML = "";
+  let selectedDate = getDate(datePicked.value);
+  let selectedRoomType = roomTypeChoice.value;
+  let availableRooms = hotel.getAvailableRooms(selectedDate, rooms, bookings, selectedRoomType);
+  availableRoomsSection.innerHTML = '';
   if (availableRooms.length > 0) {
     availableRooms.forEach((room) => {
       availableRoomsSection.innerHTML += `<section class="room-details">
-        <p class="room-detail">Room Number:${room.number}</p>
+        <p class="room-detail">Room Number: ${room.number}</p>
         <p class="room-detail">Room Type: ${room.roomType}</p>
         <p class="room-detail">Bed Size: ${room.bedSize}</p>
         <p class="room-detail">Number of Beds: ${room.numBeds}</p>
         <p class="room-detail">Has bidet: ${room.bidet}</p>
-        <p class="room-detail">Cost per Night: ${room.costPerNight}</p>
+        <p class="room-detail">Cost per Night: $${room.costPerNight}</p>
+        <button class="book-me-btn" id=${room.number}>Book me!</button>
         <br>
     </section>`;
     });
@@ -212,8 +217,9 @@ function checkAvailability() {
 }
 
 function getDate(date) {
-  
+return date.split('-').join('/');
 }
+
 
 // function setUser(username, password) {
 //   passwordField.value = "";
